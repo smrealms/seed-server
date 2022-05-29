f0eb9bfa5afdf861394d56a38f837436d74ad252
Dan Hemberger <daniel.hemberger@gmail.com>
2022-05-29 02:00:53 -0700

    setup-smr: update config option names
    See smrealms/smr@303712f4807601d7bcd9bb1e849f89b0c675f0be.

--- roles/setup-smr/files/config/config.specific.php
+++ roles/setup-smr/files/config/config.specific.php
@@ -44,7 +44,8 @@ const HISTORY_DATABASES = [
 //-------------------
 
 const DISCORD_TOKEN = '{{ smr_discord_token }}';
-const LOGGER_LEVEL = 'NOTICE';
+const DISCORD_COMMAND_PREFIX = '.';
+const DISCORD_LOGGER_LEVEL = 'NOTICE';
 
 //-------------------
 // IRC
@@ -62,12 +63,11 @@ const IRC_BOT_VERBOSE_PING = false;
 //-------------------
 
 const NPC_LOG_TO_DATABASE = false;
-
 const NPC_MAX_ACTIONS = 2500; // About a half hour worth of actions
 const NPC_LOW_TURNS = 100;
-const MINIMUM_RESERVE_CREDITS = 100000;
-const MIN_SLEEP_TIME = 800000;
-const MAX_SLEEP_TIME = 1100000;
+const NPC_MINIMUM_RESERVE_CREDITS = 100000;
+const NPC_MIN_SLEEP_TIME = 800000;
+const NPC_MAX_SLEEP_TIME = 1100000;
 
 //-------------------
 // NPC Chess
